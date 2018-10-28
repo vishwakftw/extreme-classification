1aa93533ee2b2eb2416b4a2174ffb4ee00179876
Sukrut Rao <cs15btech11036@iith.ac.in>
2018-10-28 21:21:53 +0530

    Use sklearn for agglomerative clustering
    Signed-off-by: Sukrut Rao <cs15btech11036@iith.ac.in>

--- extreme_classification/clusterings/cluster_agglomerative.py
+++ extreme_classification/clusterings/cluster_agglomerative.py
@@ -1,55 +1,34 @@
 import copy
 import numpy as np
 import os
+import sklearn
+import sklearn.cluster
 import sys
 
 from cluster import Cluster
 from cluster_utils import CoOccurrenceDistance
 
-sys.append('..')  # TODO: fix this
-from ..loaders.loader_libsvm import LibSVMLoader
+sys.path.append('..')  # TODO: fix this
+from loaders.loader_libsvm import LibSVMLoader
 
 
-class AgglomerativeClustering(object):
+class CoOccurrenceAgglomerativeClustering(object):
 
     def __init__(self, loader):
         self.class_matrix = loader.get_classes()
         self.num_data_points = len(loader)
         self.num_classes = loader.num_classes()
-        self.common_classes = self.class_matrix * self.class_matrix.T
+        self.distances = self.class_matrix * self.class_matrix.T
+        self.model = sklearn.cluster.AgglomerativeClustering(affinity='precomputed', memory=os.path.join(
+            '/', 'tmp', 'extreme_classification'), compute_full_tree=True, linkage='complete')
 
-    def get_hierarchy(self, distance_metric):
-        cluster_hierarchy = [[Cluster([i]) for i in range(self.num_data_points)]]
-        level = 0
-        while(True):
-            clusters_this_level = cluster_hierarchy[level]
-            min_distance = np.inf
-            clusters_to_merge = None
-            print("level:", level)
-            for i in range(len(clusters_this_level)):
-                for j in range(i + 1, len(clusters_this_level)):
-                    print(i, j)
-                    distance, _ = distance_metric(clusters_this_level[i], clusters_this_level[j])
-                    if distance < min_distance:
-                        min_distance = distance
-                        clusters_to_merge = (i, j)
-            assert clusters_to_merge is not None
-            clusters_next_level = copy.deepcopy(clusters_this_level)
-            clusters_next_level[clusters_to_merge[0]].merge(
-                clusters_next_level[clusters_to_merge[1]])
+    def get_clusters(self, num_clusters):
+        self.model.set_params(n_clusters=num_clusters)
+        clusters = self.model.fit_predict(self.distances.toarray())
+        return clusters
 
-            clusters_next_level.remove(clusters_next_level[clusters_to_merge[1]])
-            cluster_hierarchy.append(clusters_next_level)
-            if len(clusters_next_level) == 1:
-                break
-            level += 1
-        self.cluster_hierarchy = cluster_hierarchy
-        return cluster_hierarchy
-
-    def get_common_classes_matrix(self):
-        return self.common_classes
+    def get_distance_matrix(self):
+        return self.distances
 
 if __name__ == "__main__":
-    l = LibSVMLoader('../../data/Bibtex/Bibtex_data.txt')
-    a = AgglomerativeClustering(l)
-    print(a.get_hierarchy(CoOccurrenceDistance(a.get_common_classes_matrix())))
+    print("Performs Agglomerative Clustering using negative of co-occurrence as the distance metric")
