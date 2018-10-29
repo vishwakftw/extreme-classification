97fa781f87492bb871d60a4b88834e16aea78659
vishwakftw <cs15btech11043@iith.ac.in>
2018-10-29 14:15:04 +0530

    Add placeholders for output autoencoding

--- training_scripts/train_autoencoder.py
+++ training_scripts/train_autoencoder.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from argparse import ArgumentParser
 from extreme_classification.loaders import LibSVMLoader
 from extreme_classification.autoencoders import Autoencoder as AE
+from extreme_classification.clusterings import CoOccurrenceAgglomerativeClustering as CAAC
 
 import sys
 import yaml
@@ -13,6 +14,9 @@ parser = ArgumentParser()
 # data argument
 parser.add_argument('--data_root', type=str, required=True,
                     help='Root folder for dataset')
+parser.add_argument('--outputs', action='store_true',
+                    help='If toggled, then the autoencoder is trained on the outputs, \
+                          otherwise it is trained on the outputs')
 
 # architecture arguments
 parser.add_argument('--encoder_cfg', type=str, required=True,
@@ -54,9 +58,9 @@ if args.seed is not None:
     if using_cuda:
         torch.cuda.manual_seed(args.seed)
 
-data = LibSVMLoader(args.data_root)
+loader = LibSVMLoader(args.data_root)
 loader_kwargs = {'num_workers': 1, 'pin_memory': True} if using_cuda else {}
-data_loader = torch.utils.data.DataLoader(data, batch_size=args.batch_size, shuffle=True,
+data_loader = torch.utils.data.DataLoader(loader, batch_size=args.batch_size, shuffle=True,
                                           **loader_kwargs)
 
 model = AE(yaml.load(open(args.encoder_cfg)), yaml.load(open(args.decoder_cfg))).to(device)
@@ -64,18 +68,30 @@ model = AE(yaml.load(open(args.encoder_cfg)), yaml.load(open(args.decoder_cfg)))
 opt_config = yaml.load(open(args.optimizer_cfg))
 optimizer = getattr(torch.optim, opt_config['name'])(model.parameters(), **opt_config['args'])
 
+for_outputs = args.for_outputs
+interval = args.interval
+
+if for_outputs:
+    agglo_clusters = CAAC(loader)
+    get_permutation = agglo_clusters.get_permutation_lambda
+
 for epoch in range(0, args.epochs):
     model.train()
-    for batch_idx, (data, _) in enumerate(data_loader):
-        data = data.to(device=device, dtype=torch.float32)
+    for batch_idx, (inp, out) in enumerate(data_loader):
+        if for_outputs:
+            data = get_permutation(out).to(device=device, dtype=torch.float32)
+        else:
+            data = inp.to(device=device, dtype=torch.float32)
         optimizer.zero_grad()
         output = model(data)
         loss = torch.nn.functional.mse_loss(output, data)
         loss.backward()
         optimizer.step()
-        if batch_idx % args.interval == 0:
+        if batch_idx % interval == 0:
             print('Training epoch {} : Loss = {}'.format(epoch, loss.item()))
 
 # Transfer the model to CPU before saving
 if args.save_model:
-    torch.save(model.to('cpu'), 'trained_model_{}.pt'.format(datetime.utcnow().isoformat()))
+    split_str = 'outputs' if for_outputs else 'inputs'
+    torch.save(model.to('cpu'),
+               'trained_model_{}_{}.pt'.format(split_str, datetime.utcnow().isoformat()))
