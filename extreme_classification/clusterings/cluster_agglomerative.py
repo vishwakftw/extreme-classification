356e1d46661d47305c7752c7a724b0edc58cbc4b
Sukrut Rao <cs15btech11036@iith.ac.in>
2018-11-06 21:26:09 +0530

    Define default cluster parameter
    Signed-off-by: Sukrut Rao <cs15btech11036@iith.ac.in>
    
    * Number of clusters is number of classes if no value passed
    * Nomenclature of vector_length and num_data_points fixed

--- extreme_classification/clusterings/cluster_agglomerative.py
+++ extreme_classification/clusterings/cluster_agglomerative.py
@@ -19,17 +19,17 @@ class CoOccurrenceAgglomerativeClustering(object):
 
         """
         self.class_matrix = class_matrix
-        self.num_data_points = self.class_matrix.get_shape()[1]
-        self.vector_length = self.class_matrix.get_shape()[0]
+        self.vector_length = self.class_matrix.get_shape()[1]
+        self.num_data_points = self.class_matrix.get_shape()[0]
         self.distances = -(self.class_matrix.T * self.class_matrix)
-        self.model = sklearn.cluster.AgglomerativeClustering(n_clusters=self.num_data_points,
+        self.model = sklearn.cluster.AgglomerativeClustering(n_clusters=self.vector_length,
                                                              affinity='precomputed',
                                                              memory=os.path.join(
                                                                  '/', 'tmp', 'extreme_classification'),
                                                              compute_full_tree=True, linkage='complete')
         self.model.fit(self.distances.toarray())
 
-    def get_clusters(self, num_clusters):
+    def get_clusters(self, num_clusters=None):
         """
         @brief      Performs clustering on the classes of the dataset and returns clusters
 
@@ -38,6 +38,8 @@ class CoOccurrenceAgglomerativeClustering(object):
 
         @return     A 1D array with the ith element being the cluster index of the ith data point
         """
+        if num_clusters is None:
+            num_clusters = self.vector_length
         self.model.set_params(n_clusters=num_clusters)
         clusters = self.model.fit_predict(self.distances.toarray())
         return clusters
@@ -50,18 +52,18 @@ class CoOccurrenceAgglomerativeClustering(object):
 
         @return     A 1D array that is a permutation of the class IDs
         """
-        ordering = {i: [i] for i in range(self.num_data_points)}
+        ordering = {i: [i] for i in range(self.vector_length)}
         iterations = self.model.children_
         c = len(ordering)
-        for i in range(self.num_data_points - 1):
+        for i in range(self.vector_length - 1):
             ordering[c] = ordering[iterations[i][0]] + ordering[iterations[i][1]]
             del ordering[iterations[i][0]]
             del ordering[iterations[i][1]]
             c += 1
         return ordering[c - 1]
-        # ordering = [[i] for i in range(self.num_data_points)]
+        # ordering = [[i] for i in range(self.vector_length)]
         # iterations = self.model.children_
-        # for i in range(self.num_data_points - 1):
+        # for i in range(self.vector_length - 1):
         #     ordering.append(ordering[iterations[i][0]] + ordering[iterations[i][1]])
         # return ordering[-1]
 
