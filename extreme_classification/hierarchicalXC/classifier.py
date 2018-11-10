6b157f331023d7b91ec77c25698d3a7fd099b2c9
Sukrut Rao <cs15btech11036@iith.ac.in>
2018-11-10 19:14:28 +0530

    Add missed case where data may be in both classes
    Signed-off-by: Sukrut Rao <cs15btech11036@iith.ac.in>

--- extreme_classification/hierarchicalXC/classifier.py
+++ extreme_classification/hierarchicalXC/classifier.py
@@ -29,14 +29,22 @@ class HierarchicalXC(object):
         merge_iterations = self.cluster_creator.get_merge_iterations()
         self.classifiers = [None] * len(merge_iterations)
         for merge in range(len(merge_iterations) - 1, -1, -1):
-            data_subset = []
+        	class_indexes = []
             for i in range(2):
                 class_i_list = merge_indices[merge_iterations[merge][0]]
                 class_i_indexes = scipy.sparse.find(self.class_matrix[:, class_i_list] == 1)
-                data_subset.append(self.feature_matrix[class_i_indexes[0]])
-            train_X = scipy.sparse.vstack((data_subset[0], data_subset[1]))
-            train_y = np.hstack(
-                (np.full(data_subset[0].get_shape()[0], 0), np.full(data_subset[1].get_shape()[0], 1)))
+                class_indexes.append(class_i_indexes[0])
+            data_only_0 = np.setdiff1d(class_indexes[0],class_indexes[1],assume_unique=True)
+            data_only_1 = np.setdiff1d(class_indexes[1],class_indexes[0],assume_unique=True)
+            data_both = np.intersect1d(class_indexes[0],class_indexes[1],assume_unique=True)
+            l_0 = data_only_0.get_shape()[0]
+            l_1 = data_only_1.get_shape()[0]
+            l_both = data_both.get_shape()[0]
+            train_X = scipy.sparse.vstack((data_only_0, data_only_1, data_both))
+            train_y = np.zeros((l_0 + l_1 + l_both, 2))
+            train_y[:l_0,0] = 1
+            train_y[l_0:l_1,1] = 1
+            train_y[l_1:] = 1
             classifier = train_single_classifier(train_X, train_y)
             self.classifiers[merge] = classifier
 
